2b355e1eac7d828cf2b824ce9e3d5f885dd686ae
Tatiana <tatimagdalena@gmail.com>
2017-04-18 08:24:29 -0300

    First commit

---
+++ Plata/Plata.h
@@ -0,0 +1,19 @@
+//
+//  Plata.h
+//  Plata
+//
+//  Created by Tatiana Magdalena on 4/18/17.
+//  Copyright © 2017 Tatiana Magdalena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Plata.
+FOUNDATION_EXPORT double PlataVersionNumber;
+
+//! Project version string for Plata.
+FOUNDATION_EXPORT const unsigned char PlataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Plata/PublicHeader.h>
+
+
---
+++ PlataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlataDemo
+//
+//  Created by Tatiana Magdalena on 4/18/17.
+//  Copyright © 2017 Tatiana Magdalena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
