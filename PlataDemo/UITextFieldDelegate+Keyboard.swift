b2232c3c526848c04f4160a38d4a9db36cb5c27a
Tatiana <tatimagdalena@gmail.com>
2017-04-18 08:44:50 -0300

    Add Demo

---
+++ PlataDemo/UITextFieldDelegate+Keyboard.swift
@@ -0,0 +1,21 @@
+////
+////  UITextFieldDelegate+Keyboard.swift
+////  PlataDemo
+////
+////  Created by Tatiana Magdalena on 07/03/17.
+////  Copyright © 2017 Stone. All rights reserved.
+////
+//
+//import UIKit
+//
+//extension UITextFieldDelegate {
+//    
+//    
+//    
+//    
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
+//    
+//}
---
+++ PlataDemo/UIViewController+UITextFieldDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController+UITextFieldDelegate.swift
+//  mPOSLinkDemo
+//
+//  Created by Tatiana Magdalena on 30/03/17.
+//  Copyright © 2017 Stone. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController: UITextFieldDelegate{
+    func addToolBar(textField: UITextField){
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.black
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(UIViewController.donePressed))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        toolBar.setItems([spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        toolBar.sizeToFit()
+        
+        textField.delegate = self
+        textField.inputAccessoryView = toolBar
+    }
+    func donePressed(){
+        view.endEditing(true)
+    }
+    func cancelPressed(){
+        view.endEditing(true) // or do something
+    }
+}
